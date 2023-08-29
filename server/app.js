c6d76e93ff4130f48c28208da4ecad1d87c591ad
20215ganesh <muzinoasahi@gmail.com>
2023-08-29 11:46:55 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express=require('express');
 const dbConfig=require('./db');
 const bodyParser=require('body-parser');
 const roomsRoute=require('./routes/roomsRoute');
+const cors = require('cors');
 const userRoute=require('./routes/userRoute');
 const bookroomRoute=require('./routes/bookingRoute')
 const app=express();
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust this to match your frontend's URL
+  }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(roomsRoute);
