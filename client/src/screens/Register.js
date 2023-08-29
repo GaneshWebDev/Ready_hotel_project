1268e9d5b26b70a49eeeb25582aae693eecf3155
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2023-08-29 12:21:20 +0530

    Update Register.js

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -23,7 +23,7 @@ const Register = () => {
         }
         try {
             setloading(true);
-            axios.post('https://hotelbookingservicebackend.onrender.com/register',user).then(
+            axios.post('https://hotelbookingservice.onrender.com/register',user).then(
                 response=>{
                     console.log(`successfully create ${response}`);
                     setloading(false);
